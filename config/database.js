fc26ea74a3ff314e72b0ad5ef05a06a8f5adb3be
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-06-10 13:41:36 -0300

    settings database

--- config/database.js
+++ config/database.js
@@ -76,11 +76,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr (1))
     },
     debug: Env.get('DB_DEBUG', false)
   }
