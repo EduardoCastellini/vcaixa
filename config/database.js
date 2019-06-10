f74d3cd30a1c8a6f9170061624d1791017dd16b6
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-06-10 15:01:50 -0300

    settings

--- config/database.js
+++ config/database.js
@@ -81,7 +81,7 @@ module.exports = {
       port: Env.get('DB_PORT', DATABASE_URL.port),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr (1))
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   }
