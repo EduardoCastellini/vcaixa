4c41b0d31248d5a55572a30d73257191637ad3da
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-06-10 13:36:14 -0300

    settings database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Url = require ( 'url-parse' )
+const DATABASE_URL = new Url (Env.get ( 'DATABASE_URL' ))
+
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
